8adb6c6890a3e107b4fcd87bc5e5b7b4b7579897
Michael R <mruhaak@gmail.com>
2020-06-27 21:26:14 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,7 +1,9 @@
 def my_find(collection)
   i = 0
   while i < collection.length
-    return collection[i] if yield(collection[i])
+    if yield(collection[i])
+      return collection[i]
+    end
     i = i + 1
   end
 end
